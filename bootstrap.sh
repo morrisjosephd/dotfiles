e50f64cd75947445608d0f24688f4adafaabdda8
Joe Morris <joe@scriptdrop.co>
2020-10-21 20:12:15 -0400

    create symlinks to root

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 # Run from dotfiles project root
 
+DOTFILES_PATH=~/projects/dotfiles/
+
 # Download git-completion
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o .git-completion.bash
 wait
@@ -14,23 +16,10 @@ wait
 source brew.sh
 wait
 
-# Create symlink to .bash_profile and source it
-FILE_PATH=~/projects/dotfiles/.bash_profile
-[ -r $FILE_PATH ] && [ -f $FILE_PATH ] && ln -s $FILE_PATH ~/.bash_profile
-wait
+# Create symlinks and source
+for file in .{bash_profile,gitignore_global,gitconfig}; do
+  [ -r "$file" ] && [ -f "$file" ] && ln -sf $DOTFILES_PATH"$file" ~/"$file"
+done;
+unset file;
 
-source ~/.bash_profile
-
-# Create symlink to .gitignore_global and source it
-FILE_PATH=~/projects/dotfiles/.gitignore_global
-[ -r $FILE_PATH ] && [ -f $FILE_PATH ] && ln -s $FILE_PATH ~/.gitignore_global
-wait
-
-source ~/.gitignore_global
-
-# Create symlink to .gitconfig and source it
-FILE_PATH=~/projects/dotfiles/.gitconfig
-[ -r $FILE_PATH] && [ -f $FILE_PATH ] && ln -s $FILE_PATH ~/.gitconfig
-wait
-
-source ~/.gitconfig
+source $DOTFILES_PATH.bash_profile
